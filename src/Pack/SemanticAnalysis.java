a43dc1dc7755126bd516f520fc886c4d4219441f
edjo93 <eddas.carrasco@unitec.edu>
2021-11-07 19:52:56 -0600

    se agregan los parametros

--- src/Pack/SemanticAnalysis.java
+++ src/Pack/SemanticAnalysis.java
@@ -25,8 +25,8 @@ public class SemanticAnalysis {
     public void Traverse(Nodo nodo){
         if(nodo.getNombre().equals("VARIABLE_DECLARATION")){
             addVariableDeclaration(nodo);
-        //else if(nodo.getNombre().equals("FUNCTION")){
-
+        }else if(nodo.getNombre().equals("FUNCTION_BLOCK")){
+            addFunctionBlock(nodo);
         }else{
             for (Nodo hijos : nodo.getHijos()) {
                 Traverse(hijos);
@@ -54,23 +54,103 @@ public class SemanticAnalysis {
     public void addFunctionBlock(Nodo nodo){
         String returnType="";
         Nodo parametros=null;
+        //Nodo id_list = null;
+        
         for (Nodo hijo : nodo.getHijos()) {
             if(hijo.getNombre().equals("RETURN_TYPE")){
                 returnType = hijo.getValor();
             }
             if (hijo.getNombre().equals("MAIN_PARAMETERS")) {
-                parametros = hijo;
+                parametros = hijo; // parametros.parameters 
             }  
+            
         }
         
-        FunctionTableNode tmpfnode = new FunctionTableNode(nodo.getValor(), "s0", returnType, "s0");
+        // parametros 
+        
+       
+        
+        // por aqui deber ir la funcion para añadir los parametros
+        
+        
+        //por aqui se va agregando todo al objeto funtionTableNode
+        
+        FunctionTableNode tmpfnode = new FunctionTableNode(nodo.getHijos().get(0).getValor(), "s0", returnType, "s0");
+         
+        
+        for (int i = 0; i < parametros.getHijos().size(); i++) {
+            Nodo actualNode = parametros.getHijos().get(0); 
+            System.out.println(actualNode.getNombre());
+            agregar_parametros_a_tmpfnode(tmpfnode, actualNode);
+        }
+        
+        
+        
     }
-
+    
+    
+    /*
     public ArrayList<VariableTableNode> addParameters(Nodo nodo){
+        FuntionTableNode tmpfnode = FunctionTableNode()
+        
         for (Nodo Hijo : nodo.getHijos()) {
             
         }
+    }*/
+    
+    public void agregar_parametros_a_tmpfnode(FunctionTableNode tmpnode, Nodo nodo/*each parameter_specification*/){
+        //Se necesita el ultimo nodo para saber el tipo
+        String tipo = nodo.getHijos().get(nodo.getHijos().size()-1).getValor();
+        //Se debe iterar todos menos el ultimo tipo, debido a que no es un id 
+        String parameter_mode = nodo.getHijos().get(nodo.getHijos().size()-2).getValor();
+        
+        int parameter_mode_int ; 
+        
+        switch (parameter_mode) {
+            case "In":
+                parameter_mode_int = 1;
+                break;
+            case "Out":
+                parameter_mode_int = 2;
+                break;
+            case "In Out":
+                parameter_mode_int = 3; 
+                break;
+            default:
+                parameter_mode_int = 0;// en caso de null ahora 1
+                break;
+        }
+        
+        Nodo ID_LIST = nodo.getHijos().get(0);
+        
+        ArrayList<VariableTableNode> params = new ArrayList<>();
+        
+        //iterar sobre ID_LIST
+        for (int i = 0; i < ID_LIST.getHijos().size(); i++) {
+            Nodo actualNode = ID_LIST.getHijos().get(i);
+            VariableTableNode tmpvnode = new VariableTableNode(actualNode.getValor(), "s0", tipo,parameter_mode_int );
+            //agrega variable a lista de simbolos, retorna true si se pudo, false si ya existia
+            System.out.println(tmpvnode.getId());
+            System.out.println(tmpvnode.getType());
+            System.out.println(tmpvnode.getForm());
+            /*if (!this.symbolTable.addSymbol(tmpvnode)) {
+                System.out.println("El identificador ya esta declarado");
+            }else{
+                //si agrega
+                params.add(tmpvnode);
+            }*/
+            
+            params.add(tmpvnode);
+        }
+        
+        //set params
+        tmpnode.setParams(params);
+        
+        
     }
+    
+    
+    
 
 
 }
